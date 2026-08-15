c13177ccd04214eac0993d31d11cda4dfdec800f
hossein-hb <h.habibi.401@proton.me>
2026-08-15 16:25:40 +0330

    add sign up methods

--- src/main/java/com/playtix/sports_event_ticketing_platform/api/controller/auth/AuthController.java
+++ src/main/java/com/playtix/sports_event_ticketing_platform/api/controller/auth/AuthController.java
@@ -1,8 +1,14 @@
 package com.playtix.sports_event_ticketing_platform.api.controller.auth;
 
+import com.playtix.sports_event_ticketing_platform.domain.dto.SignUpDto;
 import com.playtix.sports_event_ticketing_platform.domain.dto.auth.AuthResponse;
 import com.playtix.sports_event_ticketing_platform.domain.dto.auth.LoginRequest;
+import com.playtix.sports_event_ticketing_platform.domain.dto.user.UserProfileDto;
 import com.playtix.sports_event_ticketing_platform.service.auth.AuthService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,4 +29,11 @@ public class AuthController {
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
         return ResponseEntity.ok(authService.login(request));
     }
+
+    @PostMapping("/signup")
+    public ResponseEntity<UserProfileDto> signUp(@Valid @RequestBody SignUpDto signUpDto) {
+        UserProfileDto createdUser = authService.registerUser(signUpDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
+
 }
