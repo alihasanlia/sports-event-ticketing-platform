2258a5f34155d206aac5bceb0439bedc9dfcace3
1274882060 <mhmhchinavi@gmail.com>
2026-08-16 02:29:08 +0330

    add Elasticsearch foundation and configuration

---
+++ src/main/java/com/playtix/sports_event_ticketing_platform/config/ElasticsearchConfig.java
@@ -0,0 +1,10 @@
+package com.playtix.sports_event_ticketing_platform.config;
+
+import org.springframework.context.annotation.Configuration;
+
+// Basic placeholder for Elasticsearch configuration. Detailed settings (hosts, credentials)
+// should be provided in application.properties (spring.elasticsearch.*)
+@Configuration
+public class ElasticsearchConfig {
+
+}
