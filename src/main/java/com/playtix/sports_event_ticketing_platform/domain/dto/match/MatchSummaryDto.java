26f7cf98538e35468d03a8d6375b41fecc59e85c
1274882060 <mhmhchinavi@gmail.com>
2026-08-16 20:36:39 +0330

    fix bugs

--- src/main/java/com/playtix/sports_event_ticketing_platform/domain/dto/match/MatchSummaryDto.java
+++ src/main/java/com/playtix/sports_event_ticketing_platform/domain/dto/match/MatchSummaryDto.java
@@ -1,8 +1,10 @@
 package com.playtix.sports_event_ticketing_platform.domain.dto.match;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 public record MatchSummaryDto(
+    UUID id,
     String homeTeam,
     String awayTeam,
     LocalDateTime matchDate,
